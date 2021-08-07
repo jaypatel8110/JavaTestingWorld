0533492da50c6dd38a551089c6e1b47e5ddfe1f9
Jay Patel <jpatel8110@gmail.com>
2021-08-07 11:17:50 -0400

    Initial commit

---
+++ src/main/java/com/test/javatestingworld/JavaTestingWorldApplication.java
@@ -0,0 +1,13 @@
+package com.test.javatestingworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestingWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestingWorldApplication.class, args);
+    }
+
+}
